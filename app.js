2d298c31ecd152f5bcfc556003565586ee754009
Bojan Tubic <bojantubic2411@gmail.com>
2020-03-24 11:57:57 +0100

    added background and improved javascript

--- app.js
+++ app.js
@@ -4,12 +4,25 @@ document.querySelector(".submit").addEventListener("click", function() {
   document.querySelector(".input").value = "";
 });
 
+addEventListener("keyup", function(event) {
+  if (event.keyCode === 13) {
+    event.preventDefault();
+    const value = document.querySelector(".input").value;
+    if (value) addTodo(value);
+    document.querySelector(".input").value = "";
+  }
+});
+
 function addTodo(text) {
   const task = document.createElement("p");
   task.classList.add("task");
   task.innerText = text;
 
   const list = document.querySelector(".list");
+  const background = document.querySelector(".background");
+  if (list.hasChildNodes) {
+    background.style.display = "none";
+  }
 
   const todo = document.createElement("li");
   todo.classList.add("todo");
@@ -36,6 +49,14 @@ function addTodo(text) {
   trashIcon.classList.add("fa-trash");
   trash.addEventListener("click", function() {
     this.parentNode.parentNode.removeChild(this.parentNode);
+  });
+
+  trash.addEventListener("click", function() {
+    if (list.hasChildNodes()) {
+      background.style.display = "none";
+    } else {
+      background.style.display = "block";
+    }
   });
 
   list.insertBefore(todo, list.childNodes[0]);
